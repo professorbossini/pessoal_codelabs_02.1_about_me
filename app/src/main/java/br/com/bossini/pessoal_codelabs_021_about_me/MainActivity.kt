a275a06c8c67e86467f6661bb288521601c26ab6
Rodrigo Bossini <professorbossini@gmail.com>
2019-12-14 12:31:45 +0700

    Yet another test.

--- app/src/main/java/br/com/bossini/pessoal_codelabs_021_about_me/MainActivity.kt
+++ app/src/main/java/br/com/bossini/pessoal_codelabs_021_about_me/MainActivity.kt
@@ -2,11 +2,13 @@ package br.com.bossini.pessoal_codelabs_021_about_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "v3.0", Toast.LENGTH_SHORT).show()
     }
 }
